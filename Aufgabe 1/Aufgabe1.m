8505e9264622c1b8820306d0528b135e6f2134e5
Tobyde <30628244+Tobyde@users.noreply.github.com>
2017-08-02 14:10:32 +0200

    Hinzufügen des 2. Aufgabenteil

--- Aufgabe 1/Aufgabe1.m
+++ Aufgabe 1/Aufgabe1.m
@@ -11,10 +11,23 @@ picture2 = im2uint8(swapbytes(picture));
 %Jede gerade Zeile umdrehen von picture2
 picture2(2:2:end,:,:) = fliplr(picture2(2:2:end,:,:));
 
-%Rot und Grün tauschen in picture2
+%Blau und Grün tauschen in picture2
 newpicture2(:,:,1) = picture2(:,:,1);
 newpicture2(:,:,2) = picture2(:,:,3);
 newpicture2(:,:,3) = picture2(:,:,2);
+
+%imshow(newpicture2)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-imshow(newpicture2)
-%bild=rgb2hsv(picture1);
+%%Testphase%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+figure(2)
+imshow(picture1)
+
+picture1hsv = rgb2hsv(picture1);
+picture1hsv(find(picture1hsv(:,:,1)>0.09 & picture1hsv(:,:,1)<0.6)) = 0;
+picture1=hsv2rgb(picture1hsv);
+
+figure(3)
+imshow(picture1)
